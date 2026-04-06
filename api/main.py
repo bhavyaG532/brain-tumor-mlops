081f963c2df02826758c754e58fc86c114de1ed1
bhavyaG532 <bhavyanjali.ganipisetty359@gmail.com>
2026-04-06 22:23:12 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,5 +1,5 @@
-from prometheus_fastapi_instrumentator import Instrumentator
 from fastapi import FastAPI, UploadFile
+from prometheus_fastapi_instrumentator import Instrumentator
 import shutil
 import numpy as np
 from PIL import Image
@@ -7,9 +7,15 @@ import tensorflow as tf
 
 app = FastAPI()
 
+# Metrics
 Instrumentator().instrument(app).expose(app)
 
+# Load trained model
 model = tf.keras.models.load_model("model.h5")
+
+@app.get("/")
+def home():
+    return {"message": "Brain Tumor Detection API is running"}
 
 @app.post("/predict")
 def predict(file: UploadFile):
