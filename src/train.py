777aa40216e7d557831e3ca3ad6ab7de9ae1c81d
bhavyaG532 <bhavyanjali.ganipisetty359@gmail.com>
2026-04-09 23:21:42 +0530

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,18 +1,28 @@
 from dataset import load_data
 from model import build_model
 
+# 1. Load data
 train, val = load_data("data/")
 
-print("Classes:", train.class_names)
+# ⚠️ Remove this if error comes
+# print("Classes:", train.class_names)
 
+# 2. Build model
 model = build_model()
 
+# 3. Compile model
 model.compile(
     optimizer='adam',
     loss='binary_crossentropy',
     metrics=['accuracy']
 )
 
-history = model.fit(train, validation_data=val, epochs=10)
+# 4. Train model
+history = model.fit(
+    train,
+    validation_data=val,
+    epochs=20   # 🔥 increased from 10 → better learning
+)
 
+# 5. Save model
 model.save("model.h5")
