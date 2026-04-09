db6454e1bedb614a04c20fd6f908855492896d6a
bhavyaG532 <bhavyanjali.ganipisetty359@gmail.com>
2026-04-09 23:17:40 +0530

    Update model.py

--- src/model.py
+++ src/model.py
@@ -4,10 +4,19 @@ def build_model():
     model = models.Sequential([
         layers.Conv2D(32, (3,3), activation='relu', input_shape=(224,224,3)),
         layers.MaxPooling2D(),
+
         layers.Conv2D(64, (3,3), activation='relu'),
         layers.MaxPooling2D(),
+
+        layers.Conv2D(128, (3,3), activation='relu'),
+        layers.MaxPooling2D(),
+
         layers.Flatten(),
+
         layers.Dense(128, activation='relu'),
+        layers.Dropout(0.5),
+
         layers.Dense(1, activation='sigmoid')
     ])
+
     return model
