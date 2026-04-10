c56aea466f581f67d8f3666c118f6aa0a02613c8
bhavyaG532 <bhavyanjali.ganipisetty359@gmail.com>
2026-04-10 07:07:29 +0530

    Update dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -4,6 +4,7 @@ IMG_SIZE = (224, 224)
 BATCH_SIZE = 64
 
 def load_data(data_dir):
+
     train = tf.keras.preprocessing.image_dataset_from_directory(
         data_dir,
         validation_split=0.2,
@@ -21,7 +22,12 @@ def load_data(data_dir):
         image_size=IMG_SIZE,
         batch_size=BATCH_SIZE
     )
+
+    # ✅ Save class names BEFORE map
+    class_names = train.class_names
+
+    # ✅ Normalize
     train = train.map(lambda x, y: (x / 255.0, y))
     val = val.map(lambda x, y: (x / 255.0, y))
-    
-    return train, val
+
+    return train, val, class_names
